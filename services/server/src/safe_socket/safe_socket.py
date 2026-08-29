eedc9cda66741ca5433177e588c3bd408eab02a3
felipefialayre <felipefialayre@gmail.com>
2026-08-29 14:02:55 -0300

    implementacion safe socket para server

--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -4,8 +4,21 @@ import socket
 
 
 def recv_all(socket: socket.socket, size):
-    return socket.recv(size)
+    n = size
+    data = b""
+    while n > 0:
+        new_data = socket.recv(n)
+        if not new_data:
+            raise ConnectionError("conexión cerrada")
+        data += new_data
+        n -= len(new_data)
+    return data
 
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    sent = 0
+    while sent < len(bytes):
+        delta_n = socket.send(bytes[sent:])
+        if not delta_n:
+            raise ConnectionError("conexión cerrada")
+        sent += delta_n
